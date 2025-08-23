ef260648b161188d99216501de8911ed58416fad
Michael Lee <michael1998lee@outlook.com>
2025-08-23 17:10:08 -0500

    Covert .py file to ipynb for better visuals

--- LinearRegression.py
+++ LinearRegression.py
@@ -18,3 +18,31 @@ medical_df
 
 medical_df.info()
 print(medical_df.describe())
+
+
+# Exploratory Analysis and Visualization
+# Install plotlty, matplotlib, and seaborn. Then import them into the the script
+
+import plotly.express as px # High-level api
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# improvising the default style and the fonts in our charts
+sns.set_style('darkgrid')
+matplotlib.rcParams['font.size'] = 14
+matplotlib.rcParams['figure.figsize'] = (10,6)
+matplotlib.rcParams['figure.facecolor'] = '#00000000'
+
+## Age
+# Visulaize the distribution of age using a histogram with 47 bins (one for each year) and a box plot.
+# Utilize plotly to make the chart interactive (Note: similar charts could be used with Seaborn)
+
+medical_df.age.describe()
+
+fig = px.histogram(medical_df,
+                   x='age',
+                   nbins=47,
+                   title='Distribution of Age')
+fig.update_layout(bargap=0.1)
+fig.show()
